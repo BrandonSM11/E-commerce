b4c91eb8bdc6dbbbd4e5d94986ad004f1ad16961
BrandonSM11 <brandon13461@gmail.com>
2025-11-04 08:25:35 -0500

    fixed

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,17 +1,17 @@
-import mongoose from "mongoose";
+// import mongoose from "mongoose";
 
-const dbConnection = async () => {
+// const dbConnection = async () => {
 
-  try {
-    const mongodbAtlas = process.env.MONGODB_URI || ""; 
-    await mongoose.connect(mongodbAtlas);
+//   try {
+//     const mongodbAtlas = process.env.MONGODB_URI || ""; 
+//     await mongoose.connect(mongodbAtlas);
 
-    console.log('DB Online');
+//     console.log('DB Online');
 
-  } catch (error) {
-    console.log(error);
-    throw new Error("Error en la base de datos - Hable con el administrador");
-  }
-};
+//   } catch (error) {
+//     console.log(error);
+//     throw new Error("Error en la base de datos - Hable con el administrador");
+//   }
+// };
 
-export default dbConnection;
+// export default dbConnection;
